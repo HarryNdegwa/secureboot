47413055b16fa48c961b7ba52d16e377f0bbd705
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-13 20:22:35 +0300

    Modified Student management controller

--- src/main/java/com/example/demo/controllers/StudentManagementControlelr.java
+++ src/main/java/com/example/demo/controllers/StudentManagementControlelr.java
@@ -1,12 +1,13 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.example.demo.models.Student;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,39 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("management/api/v1/students")
 public class StudentManagementControlelr {
 
-    private List<String> initialData() {
-        List<String> students = new ArrayList<>();
-
-        students.add("Harry");
-        students.add("Mike");
-        students.add("Jane");
-        students.add("Anne");
-        students.add("Smith");
-
-        return students;
-    }
+    List<Student> students = Arrays.asList(new Student("Mike", "mike24"), new Student("Tom", "Tom24"),
+            new Student("Linda", "Linda24"));
 
     @GetMapping("/")
     public ResponseEntity<?> studentManagement() {
-        return ResponseEntity.ok().body(initialData());
+        return ResponseEntity.ok().body(students);
     }
 
     @PostMapping("/add/student")
-    public ResponseEntity<?> registerStudent(@RequestBody String student) {
-        List<String> students = initialData();
-        String newStudent = student;
-        students.add(newStudent);
+    public ResponseEntity<?> registerStudent(@RequestBody Student student) {
+        students.add(student);
         return ResponseEntity.ok().body("Created user!");
     }
 
     @DeleteMapping("/delete/student/{id}")
-    public ResponseEntity<?> deleteStudent(@PathVariable String id) {
-        List<String> students = initialData();
-        try {
-            students.remove(id);
-        } catch (Exception e) {
-            System.out.println("Error deleting user");
-        }
+    public ResponseEntity<?> deleteStudent() {
         return ResponseEntity.ok().body("Deleted student!");
 
     }
