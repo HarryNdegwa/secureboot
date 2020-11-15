cd17d428b0eda047b27185feafd72d6ada6e9032
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-15 17:03:22 +0300

    Added authentication request class

--- src/main/java/com/example/demo/security/jwt/JwtUsernameAndPasswordAuthFilter.java
+++ src/main/java/com/example/demo/security/jwt/JwtUsernameAndPasswordAuthFilter.java
@@ -1,8 +1,12 @@
 package com.example.demo.security.jwt;
 
+import java.io.IOException;
+
 import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,6 +17,14 @@ public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthentica
     public Authentication attempAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
 
+        try {
+            UsernamePasswordAuthRequest authRequest = new ObjectMapper().readValue(request.getInputStream(),
+                    UsernamePasswordAuthRequest.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return super.attemptAuthentication(request, response);
     }
 
 }
