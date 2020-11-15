77b35507c43ee44328915df5140704820e71a4b6
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-15 17:15:30 +0300

    Finalized jwt username password auth filter

--- src/main/java/com/example/demo/security/jwt/JwtUsernameAndPasswordAuthFilter.java
+++ src/main/java/com/example/demo/security/jwt/JwtUsernameAndPasswordAuthFilter.java
@@ -8,23 +8,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final AuthenticationManager authenticationManager;
+
     @Override
-    public Authentication attempAuthentication(HttpServletRequest request, HttpServletResponse response)
-            throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
 
         try {
             UsernamePasswordAuthRequest authRequest = new ObjectMapper().readValue(request.getInputStream(),
                     UsernamePasswordAuthRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(authRequest.getUsername(),
+                    authRequest.getPassword());
+
+            return authenticationManager.authenticate(authentication);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        return super.attemptAuthentication(request, response);
     }
 
 }
